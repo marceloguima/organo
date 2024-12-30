410512d26845b2d3ca27a27123ca26242c238d0f
Marcelo guimaraes <marceloguimaraesdev@gmail.com>
2024-12-29 21:08:35 -0400

    Criando o input select

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
+import ListaSuspensa from "./componentes/ListaSuspensa";
 
 function App() {
     return (
         <div className="App">
             <Banner />
             <Formulario />
+            {/* <ListaSuspensa /> */}
+           
+
         </div>
     );
 }
